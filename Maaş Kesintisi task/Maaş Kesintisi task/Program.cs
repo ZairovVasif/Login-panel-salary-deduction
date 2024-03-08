594b0254c08037d21d1bb8f15890ed655192d812
Vasif <159883475+ZairovVasif@users.noreply.github.com>
2024-03-09 02:18:35 +0400

    Add files via upload

---
+++ Maaş Kesintisi task/Maaş Kesintisi task/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string admin = "admin";
+        string password = "123456";
+
+        Console.WriteLine("Salam, xoş gəldiniz. Şirkət hesabınızı daxil edin: ");
+        string username = Console.ReadLine();
+
+        if (username != admin)
+        {
+            Console.WriteLine("Sadəcə idarəçi giriş edə bilər.");
+        }
+        else
+        {
+            Console.WriteLine("Salam, Admin, \nŞifrəni daxil edin: ");
+            string enteredPassword = Console.ReadLine();
+
+            if (enteredPassword == password)
+            {
+                Console.WriteLine("Təbriklər, giriş etdiniz.");
+
+                CalculateDeduction();
+            }
+            else
+            {
+                Console.WriteLine("Şifrə yanlışdır.");
+            }
+        }
+    }
+
+    static void CalculateDeduction()
+    {
+        Console.WriteLine("Maaşınızı daxil edin: ");
+        int salary = Convert.ToInt32(Console.ReadLine());
+
+        int deduction = 0;
+        if (salary >= 1000 && salary <= 2500)
+        {
+            deduction = 200;
+        }
+        else if (salary >= 2501 && salary <= 4000)
+        {
+            deduction = 350;
+        }
+        else if (salary > 4000)
+        {
+            deduction = 450;
+        }
+
+        Console.WriteLine($"Maaşınızda {deduction} AZN kesinti olacaq.");
+    }
+}
